11ccdef04a9139754d8645935afb813249949f3b
James Griffin <jegriffi@uci.edu>
2015-10-09 13:25:40 -0700

    added xml for all spots activity

---
+++ app/src/main/java/griffintech/org/surfercheater/Activities/AllSpots.java
@@ -0,0 +1,19 @@
+package griffintech.org.surfercheater.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import griffintech.org.surfercheater.R;
+
+/**
+ * Created by James on 10/9/2015.
+ */
+public class AllSpots extends AppCompatActivity {
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_all_spots);
+    }
+}
---
+++ app/src/main/java/griffintech/org/surfercheater/Fragments/CurrentAllSpotsTab.java
@@ -0,0 +1,9 @@
+package griffintech.org.surfercheater.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by James on 10/9/2015.
+ */
+public class CurrentAllSpotsTab extends Fragment {
+}
